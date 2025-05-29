7d9453eff1c195233f2b381fa301c5ea270bc850
Bodenner <204929239+tbodenner@users.noreply.github.com>
2025-05-29 08:27:25 -0700

    Removed warnings and errors for the PowerShell remoting command.

--- Install-PostImageSetup.ps1
+++ Install-PostImageSetup.ps1
@@ -1216,7 +1216,7 @@ $WorkArray.ForEach({ Install-WorkItem -WorkObject $_; Start-Sleep -Milliseconds 
 
 # enabling powershell remote for powershell verion 7
 Update-Progress -Status "Enabling PowerShell v7 Remoting" -Echo $True
-Enable-PSRemoting -Force | Out-Null
+Enable-PSRemoting -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Out-Null
 
 # run gpupdate
 Update-Progress -Status "Running GP Update" -Echo $True
